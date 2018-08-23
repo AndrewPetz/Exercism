667d57b9a3c917b5257e629f8ef072fab7ab2f7c
Andrew Petz <AnPetz@ashleyfurniture.com>
2018-08-23 10:44:40 -0500

    Working on pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        bool retVal = true;
+        List<string> alphabet = new List<string>()
+        {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        };
+        foreach(char c in input)
+        {
+            if(char.IsLetter(c))
+            {
+                string s = char.ToString(c).ToLower();
+                if (!alphabet.Contains(s))
+                {
+                    retVal = false;
+                    return retVal;
+                }
+            }
+        }
+
+        return retVal;
+    }
+}
